1c6cd8906635a57bea05b2c469eb192604715818
tharasarthi-eng <tharasarthi@gmail.com>
2026-03-24 09:33:35 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+const AAPKE_SARTHI_DATA = {
+    // Format: "Topic Name": { dpp: "Link", ppt: "Link", video: "Link" }
+    "Distance & Section Formula": {
+        dpp: "https://drive.google.com/file/d/DPP_ID_HERE/view",
+        ppt: "https://drive.google.com/file/d/PPT_ID_HERE/view",
+        video: "https://www.youtube.com/watch?v=VIDEO_ID_HERE"
+    },
+    "Centroid, Incenter & Orthocenter": {
+        dpp: "#",
+        ppt: "#",
+        video: "#"
+    },
+    "Standard Equations": {
+        dpp: "#",
+        ppt: "#",
+        video: "#"
+    }
+    // You will keep adding rows here as you create content
+};
