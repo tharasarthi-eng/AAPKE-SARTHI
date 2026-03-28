8e3a0fc6acb96c9abbe35e7fef70051e67bef3ce
tharasarthi-eng <tharasarthi@gmail.com>
2026-03-28 10:47:22 -0700

    Update data.js

--- data.js
+++ data.js
@@ -2,17 +2,17 @@ const AAPKE_SARTHI_DATA = {
     // Format: "Topic Name": { dpp: "Link", ppt: "Link", video: "Link" }
     "Distance & Section Formula": {
         dpp: "https://drive.google.com/file/d/DPP_ID_HERE/view",
-        ppt: "https://drive.google.com/file/d/PPT_ID_HERE/view",
+        ppt: "https://drive.google.com/file/d/1CJfrkmCPmtpoQ90eClZ4pqAoQ9XyrYZF/view?usp=drive_link",
         video: "https://www.youtube.com/watch?v=VIDEO_ID_HERE"
     },
     "Centroid, Incenter & Orthocenter": {
         dpp: "#",
-        ppt: "#",
+        ppt: "https://drive.google.com/file/d/1CJfrkmCPmtpoQ90eClZ4pqAoQ9XyrYZF/view?usp=drive_link",
         video: "#"
     },
     "Standard Equations": {
         dpp: "#",
-        ppt: "#",
+        ppt: "https://drive.google.com/file/d/1CJfrkmCPmtpoQ90eClZ4pqAoQ9XyrYZF/view?usp=drive_link",
         video: "#"
     }
     // You will keep adding rows here as you create content
